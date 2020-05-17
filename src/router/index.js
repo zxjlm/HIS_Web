90a0c8952bf7691a12f285cf16de58b81fbff80e
harumonia <zxjlm233@gmail.com>
2020-05-17 10:46:19 +0800

    version 0.9

--- src/router/index.js
+++ src/router/index.js
@@ -145,18 +145,18 @@ export const asyncRoutes = [
   chargeRouter,
   refundRouter,
 
-  {
-    path: '/tab',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/tab/index'),
-        name: 'Tab',
-        meta: { title: 'Tab', icon: 'tab' }
-      }
-    ]
-  },
+  // {
+  //   path: '/tab',
+  //   component: Layout,
+  //   children: [
+  //     {
+  //       path: 'index',
+  //       component: () => import('@/views/tab/index'),
+  //       name: 'Tab',
+  //       meta: { title: 'Tab', icon: 'tab' }
+  //     }
+  //   ]
+  // },
 
   // {
   //   path: '/error',
